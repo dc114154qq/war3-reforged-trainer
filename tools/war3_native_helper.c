15147cd62c4153757330189323c289a5cd57b279
Yuzhuo Tang <2023240332080@buu.edu.cn>
2026-09-11 21:00:09 +0800

    handle-special-ability-level-query

--- tools/war3_native_helper.c
+++ tools/war3_native_helper.c
@@ -5024,7 +5024,17 @@ static void run_command(void) {
                             continue;
                         }
                         op->result=clone_phase=0xd00u+(uint32_t)index;
-                        if (WAR3_CLONE_VALUE(&clone_guard, get_ability_level(target, ability_id)) != source_ability_level) {
+                        int target_level_query_error=0;
+                        int target_level_value=0;
+                        __try {
+                            target_level_value=WAR3_CLONE_VALUE(&clone_guard, get_ability_level(target, ability_id));
+                        } __except (EXCEPTION_EXECUTE_HANDLER) {
+                            DWORD target_exception=GetExceptionCode();
+                            if(target_exception==0xc0000094u) target_level_query_error=1;
+                            else { clone_error=target_exception; __leave; }
+                        }
+                        if(target_level_query_error) { continue; }
+                        if (target_level_value != source_ability_level) {
                             WAR3_CLONE_VALUE(&clone_guard, set_ability_level(target, ability_id, source_ability_level));
                             if (
                                 WAR3_CLONE_VALUE(&clone_guard, get_ability_level(target, ability_id)) !=
